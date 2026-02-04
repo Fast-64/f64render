2860f8242c68775d65a661222f5cc3d5ca0c0e8b
Lila <lilaa3public@gmail.com>
2026-02-04 17:07:58 +0000

    fix black

--- mesh/gpu_batch.py
+++ mesh/gpu_batch.py
@@ -3,11 +3,17 @@ import gpu
 
 
 def create_vert_buf(
-    shader: gpu.types.GPUShader, vbo_format, buff_vert: np.ndarray, buff_norm: np.ndarray, buff_color: np.ndarray, buff_uv: np.ndarray
+    shader: gpu.types.GPUShader,
+    vbo_format,
+    buff_vert: np.ndarray,
+    buff_norm: np.ndarray,
+    buff_color: np.ndarray,
+    buff_uv: np.ndarray,
 ) -> list[gpu.types.GPUBatch]:
     def fill_attr(name: str, data: np.ndarray):
         aid = shader.attr_from_name(name)
         vbo.attr_fill(aid, data)
+
     vbo = gpu.types.GPUVertBuf(vbo_format, len(buff_vert))
     fill_attr("pos", buff_vert)
     fill_attr("inNormal", buff_norm)
